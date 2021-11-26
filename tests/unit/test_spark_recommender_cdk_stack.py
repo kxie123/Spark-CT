9afd6478b84566981ef65450ca9eeb74ee360b56
Kenny Xie <xiek@live.unc.edu>
2021-11-25 23:33:08 -0500

    Initial cdk commit

---
+++ tests/unit/test_spark_recommender_cdk_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from spark_recommender_cdk.spark_recommender_cdk_stack import SparkRecommenderCdkStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in spark_recommender_cdk/spark_recommender_cdk_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = SparkRecommenderCdkStack(app, "spark-recommender-cdk")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
